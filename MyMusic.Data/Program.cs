b5360fbe4c123b63fee474f2d25cb66f0458816f
DutchJavaDev <Borismulder@outlook.com>
2023-10-20 01:34:01 +0200

    Create Program.cs

---
+++ MyMusic.Data/Program.cs
@@ -0,0 +1,30 @@
+﻿using Npgsql;
+using Dapper;
+using System.Reflection;
+
+var connectionString = "Host=localhost;Username=postgres;Password=Kwende1995!;Database=postgres";
+
+try
+{
+    using var connection = new NpgsqlConnection(connectionString);
+    await connection.OpenAsync();
+
+    // run scripts
+    var assembly = Assembly.GetExecutingAssembly();
+    var scripts = assembly.GetManifestResourceNames().Where(i => i.EndsWith(".sql"));
+
+    foreach (var script in scripts)
+    {
+        using var stream = assembly.GetManifestResourceStream(script);
+        using var reader = new StreamReader(stream);
+
+        var command = await reader.ReadToEndAsync();
+
+        await connection.ExecuteAsync(command);
+    }
+
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
