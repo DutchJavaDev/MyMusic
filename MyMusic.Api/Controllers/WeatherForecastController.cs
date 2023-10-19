6dce28d2eac040ae37b9e01ea9acdbe3404192d0
DutchJavaDev <Borismulder@outlook.com>
2023-10-20 01:34:25 +0200

    Create WeatherForecastController.cs

---
+++ MyMusic.Api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyMusic.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
