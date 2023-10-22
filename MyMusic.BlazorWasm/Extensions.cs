15cb9d0805dcf3abf7998015f9fbef139a189282
DutchJavaDev <Borismulder@outlook.com>
2023-10-22 17:37:46 +0200

    dto

---
+++ MyMusic.BlazorWasm/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using MyMusic.BlazorWasm.Models.Search;
+
+namespace MyMusic.BlazorWasm
+{
+    public static class Extensions
+    {
+        public static List<SearcViewModel> ToViewModels(this List<Item> result)
+        {
+            return result.Select(i => new SearcViewModel
+            {
+                Title = i.snippet.title,
+                Description = i.snippet.description,
+                CoverUrl = i.snippet.thumbnails.medium.url,
+                VideoId = i.id.videoId
+            }).ToList();
+        }
+    }
+}
