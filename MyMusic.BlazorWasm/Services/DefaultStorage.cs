c4cd32e449a40eb990a467c577e4fa8adc25a9a8
DutchJavaDev <Borismulder@outlook.com>
2023-10-22 17:36:39 +0200

    storage to store data

---
+++ MyMusic.BlazorWasm/Services/DefaultStorage.cs
@@ -0,0 +1,53 @@
+﻿using Blazored.LocalStorage;
+
+namespace MyMusic.BlazorWasm.Services
+{
+    public sealed class DefaultStorage : IStorageService
+    {
+        private readonly string msulrKey = "_4nhys6";
+        private readonly string ytdakKey = "_5s8d7g";
+        private readonly string spKey = "_sd86a09as";
+
+        private readonly ISyncLocalStorageService _syncLocalStorageService;
+
+        public DefaultStorage(ISyncLocalStorageService syncLocalStorageService)
+        {
+            _syncLocalStorageService = syncLocalStorageService;
+        }
+
+        public string GetMyMusicServerURl()
+        {
+            return _syncLocalStorageService.GetItemAsString(msulrKey);
+        }
+
+        public string GetServerPassword()
+        {
+            return _syncLocalStorageService.GetItemAsString(spKey);
+        }
+
+        public string GetYouTubeDataApiKey()
+        {
+            return _syncLocalStorageService.GetItemAsString(ytdakKey);
+        }
+
+        public void SetMyMusicServerURl(string value)
+        {
+            Set(msulrKey, value);
+        }
+
+        public void SetServerPassword(string value)
+        {
+            Set(spKey, value);
+        }
+
+        public void SetYouTubeDataApiKey(string value)
+        {
+            Set(ytdakKey, value);
+        }
+
+        private void Set(string key, string value)
+        {
+            _syncLocalStorageService.SetItemAsString(key, value);
+        }
+    }
+}
---
+++ MyMusic.BlazorWasm/Services/IStorageService.cs
@@ -0,0 +1,13 @@
+﻿namespace MyMusic.BlazorWasm.Services
+{
+    public interface IStorageService
+    {
+        string GetServerPassword();
+        void SetServerPassword(string value);
+        string GetYouTubeDataApiKey();
+        void SetYouTubeDataApiKey(string value);
+
+        string GetMyMusicServerURl();
+        void SetMyMusicServerURl(string value);
+    }
+}
