cfecf6d46a721fb7f2bfb9fc481669b6fff28c5d
DutchJavaDev <Borismulder@outlook.com>
2023-10-27 19:03:54 +0200

    simple logging

---
+++ MyMusic.Api/Models/DbLogger/DbLog.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyMusic.Api.Models.DbLogger
+{
+    public sealed class DbLog
+    {
+        [Column("serial")]
+        public int? Serial { get; set; }
+
+        [Column("message")] 
+        public string? Message { get; set; }
+
+        [Column("stacktrace")]
+        public string? StackTrace { get; set; }  
+    }
+}
---
+++ MyMusic.Api/Services/DbLogger.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using MyMusic.Api.Models.DbLogger;
+using System.Data;
+
+namespace MyMusic.Api.Services
+{
+    public sealed class DbLogger
+    {
+        private readonly IDbConnection _dbConnection;
+        public DbLogger(IDbConnection dbConnection) 
+        {
+            _dbConnection = dbConnection;
+        }
+
+        public async Task LogAsync(DbLog dbLog)
+        {
+            var sql = @"INSERT INTO mymusic.exception(message, stacktrace)
+	                        VALUES (@Message, @StackTrace);";
+            var param = new 
+            {
+                dbLog.Message,
+                dbLog.StackTrace
+            };
+
+            using(_dbConnection)
+            {
+                await _dbConnection.ExecuteAsync(sql, param);
+            }
+        }
+    }
+}
