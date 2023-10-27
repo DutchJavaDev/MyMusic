cbb68bb8e5cb50f0af1892ca1512319c5616060f
DutchJavaDev <Borismulder@outlook.com>
2023-10-28 00:58:23 +0200

    Create Utils.cs

---
+++ MyMusic.Api/Utils.cs
@@ -0,0 +1,16 @@
+﻿namespace MyMusic.Api
+{
+    public static class Utils
+    {
+        public enum PipeLineState : int
+        {
+            Null,
+            Created,
+            Downloaded,
+            Converted,
+            Uploading,
+            Done,
+            Failed
+        }
+    }
+}
