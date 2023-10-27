8a2533d038279f8d65a057974769c339a49f86e7
DutchJavaDev <Borismulder@outlook.com>
2023-10-28 00:58:11 +0200

    Update Program.cs

--- MyMusic.BlazorWasm/Program.cs
+++ MyMusic.BlazorWasm/Program.cs
@@ -10,17 +10,16 @@ EnviromentProvider.SetAssembly(typeof(Program).Assembly);
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
 builder.Services.AddBlazoredLocalStorageAsSingleton();
-builder.Services.AddScoped(sp => new HttpClient());
+
+builder.Services.AddHttpClient();
 
 builder.Services.AddScoped<IStorageService>(sp => {
 
     var storageInterface = sp.GetRequiredService<ISyncLocalStorageService>();
     return new DefaultStorage(storageInterface);
 });
-
+builder.Services.AddScoped<ApiService>();
 builder.Services.AddScoped<SearchService>();
-
 
 await builder.Build().RunAsync();
