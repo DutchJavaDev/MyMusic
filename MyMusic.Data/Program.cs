ca7c9cd4605487c5751a45fd9e811a607370f09d
DutchJavaDev <Borismulder@outlook.com>
2023-10-28 00:56:49 +0200

    Update Program.cs

--- MyMusic.Data/Program.cs
+++ MyMusic.Data/Program.cs
@@ -4,17 +4,20 @@ using System.Reflection;
 using MyMusic.Common;
 
 var connectionString = EnviromentProvider.GetDatabaseConnectionString();
+using var connection = new NpgsqlConnection(connectionString);
+NpgsqlTransaction? transaction = null;
 try
 {
-    using var connection = new NpgsqlConnection(connectionString);
     await connection.OpenAsync();
+    transaction = await connection.BeginTransactionAsync();
 
     // run scripts
     var assembly = Assembly.GetExecutingAssembly();
     var scripts = assembly.GetManifestResourceNames().Where(i => i.EndsWith(".sql"));
-
+    
     foreach (var script in scripts.OrderBy(i => i[0]))
     {
+        Console.WriteLine(script);
         using var stream = assembly.GetManifestResourceStream(script);
         using var reader = new StreamReader(stream);
 
@@ -23,8 +26,15 @@ try
         await connection.ExecuteAsync(command);
     }
 
+    await transaction.CommitAsync();
+
 }
 catch (Exception e)
 {
+    if (transaction is not null)
+    {
+        await transaction.RollbackAsync();
+    }
+
     Console.WriteLine(e);
 }
