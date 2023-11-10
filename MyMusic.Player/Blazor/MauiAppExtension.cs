eb6702c9aadc546e4d7112d263ece4d3ab691177
DutchJavaDev <Borismulder@outlook.com>
2023-11-10 22:59:37 +0100

    rename added services for config page, search page and api connection

--- MyMusic.Player/Blazor/MauiAppExtension.cs
+++ MyMusic.Player/Blazor/MauiAppExtension.cs
@@ -1,17 +1,18 @@
 ﻿using Microsoft.AspNetCore.Components;
 using System.Reflection;
 using MyMusic.Player.Blazor.Attributes;
-using MyMusic.Player.Blazor.Services;
 using MyMusic.Player.Blazor.Models;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using SQLite;
 using MyMusic.Player.Storage;
+using MyMusic.Player.Blazor.Models.Search;
+using MyMusic.Player.Services;
 
 namespace MyMusic.Player.Blazor
 {
     public static class MauiAppExtension
     {
-        public static void ConfigureBlazorNavigation(this MauiAppBuilder builder) 
+        public static void ConfigureMyMusicNavigation(this MauiAppBuilder builder) 
         {
             var pages = GetBlazorPages();
 
@@ -25,8 +26,13 @@ namespace MyMusic.Player.Blazor
             // Local database
             builder.Services.TryAddTransient(sp =>
             {
-                return new SQLiteAsyncConnection(Constants.DatabaseFileName,Constants.Flags);
+                return new SQLiteAsyncConnection(Constants.DatabasePath,Constants.Flags);
             });
+
+            builder.Services.AddTransient<ConfigurationService>();
+            builder.Services.AddTransient<VideoDurationService>();
+            builder.Services.AddTransient<SearchService>();
+            builder.Services.AddTransient<ApiService>();
         }
 
         private static IEnumerable<Type> GetBlazorPages()
@@ -36,9 +42,9 @@ namespace MyMusic.Player.Blazor
             && Attribute.IsDefined(i, typeof(NavigationInfoAttribute))).ToList();
         }
 
-        public static NavLinkElement CreateNavigationLink(this NavigationInfoAttribute navigation)
+        public static NavLinkElement CreateNavigationLink(this NavigationInfoAttribute navigation, RouteAttribute route)
         {
-            NavLinkElement link = new(navigation.Icon, navigation.Href, navigation.DisplayName, navigation.Index);
+            NavLinkElement link = new(navigation.Icon, route.Template, navigation.DisplayName, navigation.Index);
 
             if (navigation.Index == 0)
             {
@@ -46,6 +52,17 @@ namespace MyMusic.Player.Blazor
             }
 
             return link;
+        }
+
+        public static List<SearchViewModel> ToViewModels(this List<Item> result)
+        {
+            return result.Select(i => new SearchViewModel
+            {
+                Title = i.snippet.title,
+                Description = i.snippet.description,
+                CoverUrl = i.snippet.thumbnails.medium.url,
+                VideoId = i.id.videoId
+            }).ToList();
         }
     }
 }
