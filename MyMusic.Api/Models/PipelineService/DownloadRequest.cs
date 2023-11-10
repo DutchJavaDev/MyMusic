1a7f9e0a18bb08013aada9c6957bc4b2b7fc55dd
DutchJavaDev <Borismulder@outlook.com>
2023-11-10 22:56:29 +0100

    moved

--- MyMusic.Api/Models/PipelineService/DownloadRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MyMusic.Api.Models.PipelineService
-{
-    public sealed class DownloadRequest
-    {
-        public string Name { get; set; }    
-        public DateTime Release { get; set; }
-        public string DownloadId { get; set; }
-    }
-}
