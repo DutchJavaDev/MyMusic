a0d4c2dbb7c8f76293c060e4c59f00ce34915ef9
DutchJavaDev <Borismulder@outlook.com>
2023-11-10 22:58:20 +0100

    rename using routeattirebute to get href

--- MyMusic.Player/Blazor/Attributes/NavigationInfoAttribute.cs
+++ MyMusic.Player/Blazor/Attributes/NavigationInfoAttribute.cs
@@ -4,14 +4,12 @@
     public class NavigationInfoAttribute : Attribute
     {
         public string Icon { get; init; }
-        public string Href { get; init; }
         public string DisplayName { get; init; }
         public int Index { get; init; }
 
-        public NavigationInfoAttribute(string icon, string href, string displayName, int index = int.MaxValue)
+        public NavigationInfoAttribute(string icon, string displayName, int index = int.MaxValue)
         {
             Icon = icon;
-            Href = href;
             DisplayName = displayName;
             Index = index;
         }
