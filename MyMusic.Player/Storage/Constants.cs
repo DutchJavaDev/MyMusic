e000eb5a295cd45bc2ea410bfb0342bc5df7ce75
DutchJavaDev <Borismulder@outlook.com>
2023-11-10 23:02:04 +0100

    sqlite config

--- MyMusic.Player/Storage/Constants.cs
+++ MyMusic.Player/Storage/Constants.cs
@@ -7,11 +7,11 @@ namespace MyMusic.Player.Storage
         public static readonly string DatabaseFileName = "mymusic.db3";
 
         public const SQLiteOpenFlags Flags =
-            SQLiteOpenFlags.ReadWrite |
             SQLiteOpenFlags.Create | // create if not exists
+            SQLiteOpenFlags.ReadWrite |
             SQLiteOpenFlags.SharedCache; // multi threaded access to database
 
-        public static string DatabasePath = 
-            Path.Combine(FileSystem.AppDataDirectory, DatabaseFileName);
+        public static string DatabasePath =
+            Path.Combine(FileSystem.Current.AppDataDirectory, DatabaseFileName);
     }
 }
---
+++ MyMusic.Player/Storage/Models/ServerConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+
+namespace MyMusic.Player.Storage.Models
+{
+    public sealed class ServerConfiguration
+    {
+        [PrimaryKey]
+        public int? Id { get; set; }
+        public string ServerUrl { get; set; }
+        public string ServerPassword { get; set; }
+        public string ApiKey { get; set; }
+    }
+}
