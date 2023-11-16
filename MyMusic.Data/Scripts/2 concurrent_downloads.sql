adca224b1b51309f4239ebb522223802bd6cb6ca
DutchJavaDev <Borismulder@outlook.com>
2023-11-16 20:27:42 +0100

    concurrent downloads, might remove later

---
+++ MyMusic.Data/Scripts/2 concurrent_downloads.sql
@@ -0,0 +1,4 @@
+-- add column to set how many downloads are allowed to be run each cycle
+set search_path to mymusic;
+alter table server_configuration
+add if not exists concurrent_downloads int not null default 2
