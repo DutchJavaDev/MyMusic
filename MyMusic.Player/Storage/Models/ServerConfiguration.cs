0b0dd038c37405b4e805a675b1ee7f338f208f71
DutchJavaDev <Borismulder@outlook.com>
2023-11-26 19:09:27 +0100

    null na

--- MyMusic.Player/Storage/Models/ServerConfiguration.cs
+++ MyMusic.Player/Storage/Models/ServerConfiguration.cs
@@ -5,7 +5,7 @@ namespace MyMusic.Player.Storage.Models
     public sealed class ServerConfiguration
     {
         [PrimaryKey]
-        public int? Id { get; set; }
+        public int Id { get; set; }
         public string ServerUrl { get; set; }
         public string ServerPassword { get; set; }
         public string ApiKey { get; set; }
