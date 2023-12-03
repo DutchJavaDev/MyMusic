b129a18a95f1e5e138a36d448af22fff6e60041b
DutchJavaDev <Borismulder@outlook.com>
2023-12-03 22:21:12 +0100

    write once abuse everywhere

---
+++ MyMusic.StorageApi/services.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"context"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/minio/madmin-go/v3"
+)
+
+func CreateMinioClient() (*madmin.AdminClient, error) {
+	endpoint := os.Getenv("storageapi_endpoint")
+	accessKeyID := os.Getenv("storageapi_user")
+	secretAccessKey := os.Getenv("storageapi_password")
+	useSSL := false
+	return madmin.New(endpoint, accessKeyID, secretAccessKey, useSSL)
+}
+
+func CreateDatabaseClient() (*pgx.Conn, error) {
+	databaseUrl := os.Getenv("storageapi_database_url")
+	return pgx.Connect(context.Background(), databaseUrl)
+}
