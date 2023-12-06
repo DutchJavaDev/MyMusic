b6028eb3ba41fbf9c5b3cd63fc4f7b9e47e0454a
DutchJavaDev <Borismulder@outlook.com>
2023-12-06 17:26:06 +0100

    Update api.go

--- MyMusic.StorageApi/api.go
+++ MyMusic.StorageApi/api.go
@@ -7,11 +7,8 @@ import (
 func main() {
 
 	r := gin.Default()
+
 	r.LoadHTMLGlob("htmlTemplates/*")
-
-	//r.GET("/mit", minioTest)
-
-	r.GET("/sev", ShowEnviromentVariables)
 
 	r.POST("/csu", CreateStorageUser)
 
