e640c48863da9981bc8b7f12aafb421abd77d8c5
DutchJavaDev <Borismulder@outlook.com>
2024-01-11 22:08:18 +0100

    Minio authentication connection

---
+++ MyMusic.Api/Services/StorageApiAccountService.cs
@@ -0,0 +1,35 @@
+﻿using MyMusic.Common;
+using Newtonsoft.Json;
+
+namespace MyMusic.Api.Services
+{
+    public class StorageApiAccountService(IHttpClientFactory http)
+    {
+        private readonly string? _endPoint = EnviromentProvider.GetStorageApiEndpoint();
+        private readonly IHttpClientFactory _httpClientFactory = http;
+
+        public Task<HttpResponseMessage> CreateReadOnlyUser(string name)
+        {
+            return CreateUser(name, Utils.MinioReadonlyPolicy);
+        }
+
+        public Task<HttpResponseMessage> CreateWriteOnlyUser(string name)
+        {
+            return CreateUser(name, Utils.MinioWriteOnlyPolicy);
+        }
+
+        private Task<HttpResponseMessage> CreateUser(string user, string policy)
+        {
+            using var client = CreateClient();
+            var body = JsonConvert.SerializeObject(new { User=user, Policy=policy });
+            return client.PostAsync("/csu", new StringContent(body));
+        }
+
+        private HttpClient CreateClient()
+        {
+            var client = _httpClientFactory.CreateClient();
+            client.BaseAddress = new Uri(_endPoint ?? "");
+            return client;
+        }
+    }
+}
