3e6bdf78c531c69c0157fadebceecd654672ea14
mulder <borismulder@outlook.com>
2024-02-23 04:38:45 -0800

    Create MusicController.cs

---
+++ MyMusic.Api/Controllers/MusicController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyMusic.Api.Services;
+
+namespace MyMusic.Api.Controllers
+{
+  public sealed class MusicController : BaseApiController
+  {
+    [HttpGet]
+    public async Task<IActionResult> Index([FromServices] MusicService musicService)
+    {
+      return Ok(await musicService.GetDownloadedMusic());
+    }
+  }
+}
