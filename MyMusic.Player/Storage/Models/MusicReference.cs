ee40c317f104bf233e5213a957d4a8474c777934
mulder <borismulder@outlook.com>
2024-02-23 04:42:08 -0800

    Create MusicReference.cs

---
+++ MyMusic.Player/Storage/Models/MusicReference.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+
+namespace MyMusic.Player.Storage.Models
+{
+  [Table("music_reference")]
+  public sealed class MusicReference
+  {
+    [PrimaryKey]
+    [AutoIncrement]
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string CoverUrl { get; set; }
+        // GUID --- lol
+    public string TrackingId { get; set; }
+  }
+}
