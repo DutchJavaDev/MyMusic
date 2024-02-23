d0efa56a5ed9c8ac3d27eb2d4c3d0de163366a36
mulder <borismulder@outlook.com>
2024-02-23 04:41:22 -0800

    Update ConfigurationService.cs

--- MyMusic.Player/Services/ConfigurationService.cs
+++ MyMusic.Player/Services/ConfigurationService.cs
@@ -3,19 +3,27 @@ using SQLite;
 
 namespace MyMusic.Player.Services
 {
-  public sealed class ConfigurationService(SQLiteAsyncConnection connection)
-  {
-    private readonly SQLiteAsyncConnection _connection = connection;
-
-    public async Task<ServerConfiguration> GetServerConfigurationAsync()
+    public sealed class ConfigurationService(SQLiteAsyncConnection connection)
     {
-      return await _connection.Table<ServerConfiguration>()
-              .FirstOrDefaultAsync(); ;
-    }
+        private readonly SQLiteAsyncConnection _connection = connection;
+        private readonly string ApiPrefix = "/api/";
 
-    public Task<int> SaveServverconfiguration(ServerConfiguration configuration)
-    {
-      return _connection.UpdateAsync(configuration);
+        public async Task<ServerConfiguration> GetServerConfigurationAsync()
+        {
+            return await _connection.Table<ServerConfiguration>()
+                    .FirstOrDefaultAsync(); ;
+        }
+
+        public Task<int> SaveServverconfiguration(ServerConfiguration configuration)
+        {
+            return _connection.UpdateAsync(configuration);
+        }
+
+        public async Task<string> GetBaseApiUrl()
+        {
+            var configuration = await GetServerConfigurationAsync();
+
+            return $"{configuration.ServerUrl}{ApiPrefix}";
+        }
     }
-  }
 }
