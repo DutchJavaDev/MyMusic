68b533eb00cc133534fe7e9e4d77f6a4209b3565
mulder <borismulder@outlook.com>
2024-02-23 04:39:17 -0800

    Create MusicDto.cs

---
+++ MyMusic.Common/Models/MusicDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMusic.Common.Models
+{
+  public class MusicDto
+  {
+    public int Serial { get; set; }
+    public string Name { get; set; }
+    public Guid TrackingId { get; set; }
+  }
+}
