ea0930485b98ba92307d553a2228ee6c2c63fa49
mulder <borismulder@outlook.com>
2024-02-23 04:38:40 -0800

    Create StreamController.cs

---
+++ MyMusic.Api/Controllers/StreamController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MyMusic.Api.Services;
+using MyMusic.Common.Models;
+using System.Net;
+
+namespace MyMusic.Api.Controllers
+{
+    [AllowAnonymous]
+    public sealed class StreamController(MyMusicCollectionService collectionService) : BaseApiController
+    {
+        [HttpGet("apg7/{id}")]
+        public async Task<IActionResult> Stream(string id)
+        {
+            var trackId = Guid.Parse(id);
+
+            var track = await collectionService.GetByTrackId(trackId);
+
+            return File(track.BinaryData.AsByteArray, "audio/wav");
+        }
+    }
+}
