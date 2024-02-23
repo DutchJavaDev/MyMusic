7b6161fed0eb4db2702e1b730b6dd1c3bdbb1412
mulder <borismulder@outlook.com>
2024-02-23 04:38:59 -0800

    Update DownloadService.cs

--- MyMusic.Api/Services/DownloadService.cs
+++ MyMusic.Api/Services/DownloadService.cs
@@ -5,62 +5,64 @@ using static MyMusic.Common.CommonData;
 
 namespace MyMusic.Api.Services
 {
-    public sealed class DownloadService
+  public sealed class DownloadService
+  {
+    private readonly DbLogger _logger;
+    private readonly IDbConnection _connection;
+    private readonly MusicService _musicService;
+
+    public DownloadService(
+        DbLogger logger,
+        IDbConnection connection,
+        MusicService music)
     {
-        private readonly DbLogger _logger;
-        private readonly IDbConnection _connection;
-        private readonly MusicService _musicService;
+      _connection = connection;
+      _musicService = music;
+      _logger = logger;
+    }
 
-        public DownloadService(
-            DbLogger logger,
-            IDbConnection connection,
-            MusicService music) 
+    public async Task<Guid> CreateDownloadRequestAsync(DownloadRequest request)
+    {
+      using (_connection)
+      {
+        _connection.Open();
+        using var transaction = _connection.BeginTransaction();
+        try
         {
-            _connection = connection;
-            _musicService = music;
-            _logger = logger;
+          var (serial, trackingId) = await _musicService
+              .TryInsertMusicPreDownloadAsync(request.Name, transaction.Connection ?? _connection);
+
+          await InsertDownloadAsync(request, transaction, serial);
+
+          return trackingId;
         }
-
-        public async Task<int> CreateDownloadRequestAsync(DownloadRequest request)
+        catch (Exception e)
         {
-            using (_connection) 
-            {
-                _connection.Open();
-                using var transaction = _connection.BeginTransaction();
-                try
-                {
-                    var music_id = await _musicService
-                        .TryInsertMusicPreDownloadAsync(request.Name, transaction.Connection ?? _connection);
-
-                    return await InsertDownloadAsync(request, transaction, music_id);
-                }
-                catch (Exception e)
-                {
-                    transaction.Rollback();
-                    await _logger.LogAsync(e, messagePrefix: "Failed to create download request");
-                    return -1;
-                }
-            }
+          transaction.Rollback();
+          await _logger.LogAsync(e, messagePrefix: "Failed to create download request");
+          return Guid.Empty;
         }
+      }
+    }
 
-        private async Task<int> InsertDownloadAsync(DownloadRequest request, IDbTransaction transaction, int musicId)
-        {
-            var state = (int)Mp3State.Created;
-            var videoId = request.VideoId;
+    private async Task<int> InsertDownloadAsync(DownloadRequest request, IDbTransaction transaction, int musicId)
+    {
+      var state = (int)Mp3State.Created;
+      var videoId = request.VideoId;
 
-            var query = @"INSERT INTO download(music_serial, state, video_id)
+      var query = @"INSERT INTO download(music_serial, state, video_id)
 	                    VALUES (@musicId, @state, @videoId);";
 
-            var param = new
-            {
-                musicId,
-                state,
-                videoId,
-            };
+      var param = new
+      {
+        musicId,
+        state,
+        videoId,
+      };
 
-            var id = await _connection.ExecuteScalarAsync<int>(query, param,transaction);
-            transaction.Commit();
-            return id;
-        }
+      var id = await _connection.ExecuteScalarAsync<int>(query, param, transaction);
+      transaction.Commit();
+      return id;
     }
+  }
 }
