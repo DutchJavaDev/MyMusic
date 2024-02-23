e7c3d6c3a7aabe1e7ebc96f4bd8f9046d040dc8d
mulder <borismulder@outlook.com>
2024-02-23 04:39:12 -0800

    Update MusicService.cs

--- MyMusic.Api/Services/MusicService.cs
+++ MyMusic.Api/Services/MusicService.cs
@@ -1,30 +1,55 @@
 ﻿using Dapper;
+using MyMusic.Common.Models;
 using System.Data;
+using static MyMusic.Common.CommonData;
 
 namespace MyMusic.Api.Services
 {
-  public sealed class MusicService
-  {
-    /// <summary>
-    /// Purepose is to have a placeholder inplace until conversion is done and music is stored in storage
-    /// </summary>
-    /// <param name="name"></param>
-    /// <returns></returns>
-    public async Task<int> TryInsertMusicPreDownloadAsync(string name, IDbConnection connection)
+    public sealed class MusicService(IDbConnection _connection, DbLogger _dbLogger)
     {
-      const string query = @"INSERT INTO music(name, release_date)
-	                    VALUES (@name, @date) returning serial;";
+        /// <summary>
+        /// Purepose is to have a placeholder inplace until conversion is done and music is stored in storage
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public async Task<(int serial, Guid trakingId)> TryInsertMusicPreDownloadAsync(string name, IDbConnection connection)
+        {
+            const string query = @"INSERT INTO music(name, release_date)
+	                    VALUES (@name, @date) returning (serial, tracking_id);";
 
-      var param = new
-      {
-        name,
-        date = DateTime.Now
-      };
+            var param = new
+            {
+                name,
+                date = DateTime.Now
+            };
 
-      var result = await connection.ExecuteScalarAsync<string?>(query, param);
+            var inserts = await connection.ExecuteScalarAsync<object[]>(query, param);
 
-      return int.Parse(result?.ToString() ?? string.Empty);
+            var serial = (int)inserts[0];
+
+            var guid = (Guid)inserts[1];
+
+            return (serial, guid);
+        }
+
+        public async Task<object> GetDownloadedMusic()
+        {
+            try
+            {
+                var query = @"select m.name Name, m.tracking_id TrackingId from
+                              music as m
+                              left join download as d ON d.music_serial = m.serial
+                              where d.state = @state".Trim();
+
+                var param = new { state = (int)Mp3State.Uploaded };
+
+                return await _connection.QueryAsync<MusicDto>(query, param);
+            }
+            catch (Exception e)
+            {
+                _dbLogger.LogAsync(e);
+                return null;
+            }
+        }
     }
-
-  }
 }
