0dd0f732b650559833064c649a875bf474c4bb6f
mulder <borismulder@outlook.com>
2024-02-27 14:46:51 -0800

    Update MusicReference.cs

--- MyMusic.Player/Storage/Models/MusicReference.cs
+++ MyMusic.Player/Storage/Models/MusicReference.cs
@@ -2,19 +2,23 @@
 
 namespace MyMusic.Player.Storage.Models
 {
-    [Table("music_reference")]
-    public sealed class MusicReference
-    {
-        [PrimaryKey]
-        [AutoIncrement]
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string CoverUrl { get; set; }
-        // GUID --- lol
-        public string TrackingId { get; set; }
-        [Ignore]
-        public TimeSpan Durration { get; set; }
-        [Ignore]
-        public string VideoId { get; set }
-    }
+  [Table("music_reference")]
+  public sealed class MusicReference
+  {
+    [PrimaryKey]
+    [AutoIncrement]
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+    public string CoverUrl { get; set; }
+
+    // GUID --- lol
+    public string TrackingId { get; set; }
+
+    [Ignore]
+    public TimeSpan Durration { get; set; }
+
+    [Ignore]
+    public string VideoId { get; set; }
+  }
 }
