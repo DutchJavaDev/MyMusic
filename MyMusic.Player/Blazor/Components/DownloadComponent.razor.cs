afc6ec3f704a2fe3ff6bbe7a8fb631b76547a620
mulder <borismulder@outlook.com>
2024-02-27 14:46:22 -0800

    Update DownloadComponent.razor.cs

--- MyMusic.Player/Blazor/Components/DownloadComponent.razor.cs
+++ MyMusic.Player/Blazor/Components/DownloadComponent.razor.cs
@@ -1,38 +1,38 @@
 ﻿using Microsoft.AspNetCore.Components;
+using MyMusic.Common.Models;
 using MyMusic.Player.Services;
 using MyMusic.Player.Storage.Models;
-using MyMusic.Common.Models;
 
 namespace MyMusic.Player.Blazor.Components
 {
-    public partial class DownloadComponent : ComponentBase
+  public partial class DownloadComponent : ComponentBase
+  {
+    [Parameter]
+    public MusicReference Model { get; set; }
+
+    [Inject]
+    private ApiService ApiService { get; set; }
+
+    [Inject]
+    private MusicReferenceService MusicReferenceService { get; set; }
+
+    public async Task SendDownLoadAsync()
     {
-        [Parameter]
-        public MusicReference Model { get; set; }
+      var request = new DownloadRequest
+      {
+        Name = Model.Name,
+        VideoId = Model.VideoId,
+        
+      };
 
-        [Inject]
-        private ApiService ApiService { get; set; }
+      var trackingId = await ApiService.DownloadAsync(request);
 
-        [Inject]
-        private MusicReferenceService MusicReferenceService { get; set; }
-
-
-        public async Task SendDownLoadAsync() 
-        {
-            var request = new DownloadRequest
-            {
-                Name = Model.Name,
-                VideoId = Model.VideoId
-            };
-
-            var trackingId = await ApiService.DownloadAsync(request);
-
-            await MusicReferenceService.InsertAsync(new MusicReference 
-            {
-                TrackingId = trackingId,
-                CoverUrl = Model.CoverUrl,
-                Name = Model.Name,
-            });
-        }
+      await MusicReferenceService.InsertAsync(new MusicReference
+      {
+        TrackingId = trackingId,
+        CoverUrl = Model.CoverUrl,
+        Name = Model.Name,
+      });
     }
+  }
 }
