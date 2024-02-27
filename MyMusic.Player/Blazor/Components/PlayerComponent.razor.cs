f1a7d64709d136a4eef48a25bfa15659aa7c3742
mulder <borismulder@outlook.com>
2024-02-27 14:46:31 -0800

    Create PlayerComponent.razor.cs

---
+++ MyMusic.Player/Blazor/Components/PlayerComponent.razor.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using MyMusic.Player.Services;
+
+namespace MyMusic.Player.Blazor.Components
+{
+  public partial class PlayerComponent : ComponentBase, IDisposable
+  {
+    [Inject]
+    public IJSRuntime JSRuntime { get; set; }
+
+    protected DotNetObjectReference<PlayerComponent> _componentReference;
+
+    public float DurationInSeconds { get; set; }
+    public string DurationString { get; set; }
+
+    public float CurrentTimeInSeconds { get; set; }
+    public string CurrentTimeString { get; set; }
+
+    [JSInvokable]
+    public void SetDurations(float seconds)
+    {
+      DurationInSeconds = seconds;
+      TimeSpan timeSpan = TimeSpan.FromSeconds(DurationInSeconds);
+      DurationString = $"{(int)timeSpan.TotalMinutes}:{timeSpan.Seconds:D2}";
+      StateHasChanged();
+    }
+    [JSInvokable]
+    public void SetCurrentTime(float time)
+    {
+      CurrentTimeInSeconds = time;
+      TimeSpan timeSpan = TimeSpan.FromSeconds(CurrentTimeInSeconds);
+      CurrentTimeString = $"{(int)timeSpan.TotalMinutes}:{timeSpan.Seconds:D2}";
+      StateHasChanged();
+    }
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+      await base.OnAfterRenderAsync(firstRender);
+
+      if (firstRender)
+      {
+        // warning about memory
+        _componentReference = DotNetObjectReference.Create(this);
+        await JSRuntime.InvokeVoidAsync("window.setPlayerComponentReference", _componentReference);
+      }
+    }
+
+    // This method will be called when the component is disposed
+    protected virtual void Dispose(bool disposing)
+    {
+      if (disposing)
+      {
+        _componentReference.Dispose();
+      }
+    }
+    public void Dispose()
+    {
+      Dispose(disposing: true);
+      GC.SuppressFinalize(this);
+    }
+  }
+}
