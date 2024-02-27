00b2f41f209736b887b0a21ccca0b0e27b3c8a37
mulder <borismulder@outlook.com>
2024-02-27 14:46:27 -0800

    Update MusicComponent.razor.cs

--- MyMusic.Player/Blazor/Components/MusicComponent.razor.cs
+++ MyMusic.Player/Blazor/Components/MusicComponent.razor.cs
@@ -1,10 +1,8 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using MyMusic.Player.Services;
 using MyMusic.Player.Storage.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace MyMusic.Player.Blazor.Components
 {
@@ -12,5 +10,26 @@ namespace MyMusic.Player.Blazor.Components
   {
     [Parameter]
     public MusicReference Model { get; set; }
+    [Parameter]
+    public string BaseUrl { get; set; } = string.Empty;
+
+    [Inject]
+    public ApiService ApiService { get; set; }
+
+    [Inject]
+    public ConfigurationService ConfigurationService { get; set; }
+
+    [Inject]
+    public MusicReferenceService MusicReferenceService { get; set; }
+
+    [Inject]
+    public IJSRuntime jSRuntime { get; set; }
+
+    async Task PlayAsync()
+    {
+      var url = $"{BaseUrl}stream/apg7/{Model.TrackingId}";
+
+      await jSRuntime.InvokeVoidAsync("window.play", url);
+    }
   }
 }
