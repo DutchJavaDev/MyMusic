b16b0bda8f6c5d984764099cf8c34eb9fd790703
mulder <borismulder@outlook.com>
2024-03-07 12:44:12 -0800

    Update StreamController.cs

--- MyMusic.Api/Controllers/StreamController.cs
+++ MyMusic.Api/Controllers/StreamController.cs
@@ -10,11 +10,26 @@ namespace MyMusic.Api.Controllers
     [HttpGet("apg7/{id}")]
     public async Task<IActionResult> Stream(string id)
     {
+      var response = new HttpResponseMessage();
+      
       var trackId = Guid.Parse(id);
 
       var track = await collectionService.GetByTrackId(trackId);
 
-      return File(track.BinaryData.AsByteArray, "audio/wav");
+      if(track == null)
+      {
+        response.StatusCode = System.Net.HttpStatusCode.NotFound;
+        return NotFound();
+      }
+
+      return CreateFileEnabledRangeProcessing(track.BinaryData.AsByteArray);
+    }
+
+    private FileContentResult CreateFileEnabledRangeProcessing(byte[] data)
+    {
+      var file = File(data, "audio/mpeg");
+      file.EnableRangeProcessing = true; // Allow for ajusting the playback time: https://stackoverflow.com/questions/48711209/partial-content-in-net-core-mvc-for-video-audio-streaming
+      return file;
     }
   }
 }
