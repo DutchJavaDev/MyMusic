47722ef60245f6a405a18a79343e47e773dd127c
mulder <borismulder@outlook.com>
2024-03-14 11:00:25 -0700

    lol

---
+++ MyMusic.Common/Services/DatabaseLogger.cs
@@ -0,0 +1,65 @@
+﻿using Dapper;
+using System.Data;
+using System.Reflection;
+
+namespace MyMusic.Common.Services
+{
+  public sealed class DatabaseLogger(IDbConnection connection) : IDisposable
+  {
+    private readonly string? AppName = Assembly.GetExecutingAssembly().FullName;
+    private const string query = "INSERT INTO exception (message,app,stacktrace) VALUES(@message,@app,@stacktrace)";
+
+    public async Task LogExceptionAsync(Exception exception)
+    {
+      var parameters = CreateExceptionParameters(exception.Message, exception.StackTrace ?? string.Empty);
+
+      await ExecuteAsync(query, parameters);
+    }
+
+    public async Task LogAsync(string message, string stacktrace)
+    {
+      var parameters = CreateParameters(message, stacktrace);
+
+      await ExecuteAsync(query, parameters);
+    }
+
+    private object CreateExceptionParameters(string message, string stacktrace = "No stacktrace")
+    {
+      return new
+      {
+        app = AppName,
+        message = $"Exception: {message}",
+        stacktrace
+      };
+    }
+
+    private object CreateParameters(string message, string stacktrace = "No stacktrace")
+    {
+      return new
+      {
+        app = AppName,
+        message = $"Message: {message}",
+        stacktrace
+      };
+    }
+
+    private async Task ExecuteAsync(string query, object parameters)
+    {
+      if(connection.State != ConnectionState.Open)
+      {
+        connection.Open();
+      }
+
+      await connection.ExecuteAsync(query, parameters);
+    }
+
+    public void Dispose()
+    {
+      if(connection.State.Equals(ConnectionState.Open))
+      {
+        connection.Close();
+        connection.Dispose();
+      }
+    }
+  }
+}
