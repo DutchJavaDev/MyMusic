b41a97c395082238d1171b9bdb798d0a10533cf8
mulder <borismulder@outlook.com>
2024-05-09 14:40:09 -0700

    redo

---
+++ MyMusic.Player/Blazor/Components/SearchComponent.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MyMusic.Player.Blazor.Components
+{
+  public partial class SearchComponent : ComponentBase
+  {
+    [Inject]
+    private NavigationManager NavigationManager { get; set; }
+    public string Query { get; set; } = string.Empty;
+
+    private bool _ready = false;
+
+    protected override void OnParametersSet()
+    {
+      _ready = true;
+    }
+
+    public void NavigateToSearchPage()
+    {
+      if (_ready && !string.IsNullOrEmpty(Query))
+      {
+        NavigationManager.NavigateTo($"/search/{Query}");
+
+        Query = string.Empty; // ?
+      }
+    }
+  }
+}
