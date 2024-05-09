1d3274751526b7c6441d5fb720cf7f34a371df5d
mulder <borismulder@outlook.com>
2024-05-09 14:41:19 -0700

    search page

---
+++ MyMusic.Player/Pages/Search.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MyMusic.Player.Pages
+{
+  public partial class Search : ComponentBase
+  {
+    [Parameter]
+    public string SearchQuery { get; set; } = string.Empty;
+
+    public bool HasSearchQuery { get; set; }
+
+    protected override void OnParametersSet()
+    {
+      HasSearchQuery = !string.IsNullOrEmpty(SearchQuery);
+    }
+  }
+}
