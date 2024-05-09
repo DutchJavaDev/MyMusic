70b94094692aeb6be1f719c2ceb5290f9a85cfa0
mulder <borismulder@outlook.com>
2024-05-09 14:41:55 -0700

    Ui overhaul
    redid main UI, using radzen packge kinda like it

---
+++ MyMusic.Player/Shared/MainLayout.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MyMusic.Player.Shared
+{
+  public partial class MainLayout : LayoutComponentBase
+  {
+    // Load playlists id's
+    public Dictionary<string, int> TemplPlaylist = new(); 
+
+    public bool SidebarExpanded { get; set; } = true;
+
+    protected override void OnInitialized()
+    {
+      for (int i = 0; i < 5; i++)
+      {
+        TemplPlaylist.Add($"Playlist_{i}",i);
+      }
+    }
+  }
+}
