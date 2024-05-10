835d253940d9154dc5dedf20eb67eab6682a48e8
mulder <borismulder@outlook.com>
2024-05-10 16:19:09 -0700

    Create SongDemo.cs

---
+++ MyMusic.Player/Blazor/Models/Temp/SongDemo.cs
@@ -0,0 +1,10 @@
+﻿namespace MyMusic.Player.Blazor.Models.Temp
+{
+  public sealed class SongDemo
+  {
+    public int Serial { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public TimeSpan Durration { get; set; } 
+  }
+}
