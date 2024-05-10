541b161ea6e85ddfcfc2ba74ba0b3b7fad2a04b8
mulder <borismulder@outlook.com>
2024-05-10 16:18:46 -0700

    demo playlist

---
+++ MyMusic.Player/Blazor/Components/PlaylistsComponent.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MyMusic.Player.Blazor.Components
+{
+  public partial class PlaylistsComponent : ComponentBase
+  {
+    private readonly List<string> Playlists = [];
+
+    protected override void OnInitialized()
+    {
+      for (int i = 0; i < 30; i++)
+      {
+        Playlists.Add($"Playlist_{i}");
+      }
+    }
+  }
+}
