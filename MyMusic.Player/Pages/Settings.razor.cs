528ac8b673b84614c7e732cae863a9c87dbaf848
mulder <borismulder@outlook.com>
2024-05-10 16:19:29 -0700

    wip

--- MyMusic.Player/Pages/Settings.razor.cs
+++ MyMusic.Player/Pages/Settings.razor.cs
@@ -1,8 +1,42 @@
 ﻿using Microsoft.AspNetCore.Components;
+using MyMusic.Player.Blazor.Models.Temp;
 
 namespace MyMusic.Player.Pages
 {
   public partial class Settings : ComponentBase
   {
+    public ConfigurationDemo CurrentConfiguration { get; set; }
+
+    private IEnumerable<ConfigurationDemo> _configurations;
+    private IEnumerable<string> _configurationsNames;
+    private string _selectedConfig;
+
+    protected override void OnInitialized()
+    {
+      _configurations = Enumerable.Range(2, 6).Select(i => new ConfigurationDemo
+      { 
+        Serial = i,
+        Name = $"Config_{i}",
+        CLoudPassword = Guid.NewGuid().ToString(),
+        CloudUrl = $"localhost:{i*i}",
+        DataApiKey = Guid.NewGuid().ToString(),
+      });
+
+      CurrentConfiguration = _configurations.First();
+
+      _configurationsNames = _configurations.Select(i => i.Name);
+
+      _selectedConfig = _configurationsNames.First();
+    }
+
+    private void Submit()
+    {
+
+    }
+
+    private void ChangeConfig(string name)
+    {
+      CurrentConfiguration = _configurations.Where(i => i.Name == name).FirstOrDefault();
+    }
   }
 }
