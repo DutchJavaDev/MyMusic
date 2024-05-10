11c89e19172fdf1b3b56c3e0652792d026e5eb15
mulder <borismulder@outlook.com>
2024-05-10 16:19:05 -0700

    Create ConfigurationDemo.cs

---
+++ MyMusic.Player/Blazor/Models/Temp/ConfigurationDemo.cs
@@ -0,0 +1,11 @@
+﻿namespace MyMusic.Player.Blazor.Models.Temp
+{
+  public sealed class ConfigurationDemo
+  {
+    public int Serial {  get; set; }  
+    public string Name { get; set; }
+    public string CloudUrl {  get; set; }
+    public string CLoudPassword { get; set; }
+    public string DataApiKey { get; set; } 
+  }
+}
