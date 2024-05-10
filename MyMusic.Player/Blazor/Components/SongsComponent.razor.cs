bfa381d18e0195223838297b701bd03eff40aced
mulder <borismulder@outlook.com>
2024-05-10 16:18:58 -0700

    demo songs

---
+++ MyMusic.Player/Blazor/Components/SongsComponent.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using MyMusic.Player.Blazor.Models.Temp;
+
+namespace MyMusic.Player.Blazor.Components
+{
+  public partial class SongsComponent : ComponentBase
+  {
+    private IEnumerable<SongDemo> songDemos;
+
+    protected override void OnInitialized()
+    {
+      songDemos = Enumerable.Range(0,49).Select(i => new SongDemo 
+      {
+        Serial = i,
+        Description = $"Description_{i}",
+        Durration = TimeSpan.FromMinutes(i),
+        Name = $"Song_{i}"
+      });
+    }
+  }
+}
