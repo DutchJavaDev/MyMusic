8da951067c9c1b6dd8a6834d8a5e36b11f5d4b67
mulder <borismulder@outlook.com>
2024-05-14 10:28:10 -0700

    Update Search.razor.cs

--- MyMusic.Player/Pages/Search.razor.cs
+++ MyMusic.Player/Pages/Search.razor.cs
@@ -1,17 +1,32 @@
 ﻿using Microsoft.AspNetCore.Components;
+using MyMusic.Player.Services;
 
 namespace MyMusic.Player.Pages
 {
   public partial class Search : ComponentBase
   {
+    [Inject]
+    public SearchService SearchService { get; set; }
+
     [Parameter]
     public string SearchQuery { get; set; } = string.Empty;
 
     public bool HasSearchQuery { get; set; }
 
+    public string SearchResponse { get; set; } = string.Empty;
+
     protected override void OnParametersSet()
     {
       HasSearchQuery = !string.IsNullOrEmpty(SearchQuery);
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+      if (HasSearchQuery || !string.IsNullOrEmpty(SearchQuery))
+      {
+        SearchResponse = await SearchService.SearchRawResults(SearchQuery);
+        StateHasChanged();
+      }
     }
   }
 }
