ce139db0eb9ad6f2a59bfc1d06d9d182bd526106
mulder <borismulder@outlook.com>
2024-05-14 10:27:43 -0700

    Update ConfigurationDemo.cs

--- MyMusic.Player/Blazor/Models/Temp/ConfigurationDemo.cs
+++ MyMusic.Player/Blazor/Models/Temp/ConfigurationDemo.cs
@@ -5,7 +5,7 @@
     public int Serial {  get; set; }  
     public string Name { get; set; }
     public string CloudUrl {  get; set; }
-    public string CLoudPassword { get; set; }
+    public string CloudPassword { get; set; }
     public string DataApiKey { get; set; } 
   }
 }
