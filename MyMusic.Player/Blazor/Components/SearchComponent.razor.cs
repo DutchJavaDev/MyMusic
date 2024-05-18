f7bf31c256d16eea093c7fd725eba089a1794f5a
mulder <borismulder@outlook.com>
2024-05-18 11:16:44 -0700

    Update SearchComponent.razor.cs

--- MyMusic.Player/Blazor/Components/SearchComponent.razor.cs
+++ MyMusic.Player/Blazor/Components/SearchComponent.razor.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using MyMusic.Player.Pages;
+using MyMusic.Player.Services;
 
 namespace MyMusic.Player.Blazor.Components
 {
@@ -8,21 +11,25 @@ namespace MyMusic.Player.Blazor.Components
     private NavigationManager NavigationManager { get; set; }
     public string Query { get; set; } = string.Empty;
 
-    private bool _ready = false;
-
-    protected override void OnParametersSet()
-    {
-      _ready = true;
-    }
+		public void CheckKeyPressed(KeyboardEventArgs keyboardEventArgs) 
+		{
+			if (keyboardEventArgs.Code == "Enter")
+			{
+				NavigateToSearchPage();
+			}
+		}
 
     public void NavigateToSearchPage()
     {
-      if (_ready && !string.IsNullOrEmpty(Query))
+      if (!string.IsNullOrEmpty(Query) && !NavigationManager.Uri.Contains("search"))
       {
         NavigationManager.NavigateTo($"/search/{Query}");
-
-        Query = string.Empty; // ?
       }
+			else
+			{
+				// Need to notify search page when we are already on the search page
+				PageNotificationService.InvokeActionCallBackFor(typeof(Search), Query);
+			}
     }
   }
 }
