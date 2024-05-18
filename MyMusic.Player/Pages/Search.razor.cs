44dcbf64ad82b43b036e99cfbbd91f1c3f9c42b5
mulder <borismulder@outlook.com>
2024-05-18 11:18:51 -0700

    Simpler search with cancelation token, callback that gets triggerd from the searchcomponent

--- MyMusic.Player/Pages/Search.razor.cs
+++ MyMusic.Player/Pages/Search.razor.cs
@@ -1,32 +1,86 @@
 ﻿using Microsoft.AspNetCore.Components;
 using MyMusic.Player.Services;
+using MyMusic.Player.Services.Youtube;
+using MyMusic.Player.Services.Youtube.Models;
 
 namespace MyMusic.Player.Pages
 {
-  public partial class Search : ComponentBase
+	public partial class Search : ComponentBase, IDisposable
   {
-    [Inject]
-    public SearchService SearchService { get; set; }
+		private bool disposedValue;
+
+		private CancellationTokenSource cancellationTokenSource;
+
+		[Inject]
+    private YoutubeSearchService YoutubeSearchService { get; set; }
 
     [Parameter]
     public string SearchQuery { get; set; } = string.Empty;
 
-    public bool HasSearchQuery { get; set; }
-
-    public string SearchResponse { get; set; } = string.Empty;
-
-    protected override void OnParametersSet()
-    {
-      HasSearchQuery = !string.IsNullOrEmpty(SearchQuery);
-    }
+    private YouTubeSearchModel Model { get; set; } 
 
     protected override async Task OnInitializedAsync()
     {
-      if (HasSearchQuery || !string.IsNullOrEmpty(SearchQuery))
-      {
-        SearchResponse = await SearchService.SearchRawResults(SearchQuery);
-        StateHasChanged();
-      }
+			// Callback to update page from other components
+			PageNotificationService.AddActionCallBack(GetType(), async (data) => { await RemoteSearchAsync(data); });
+
+			cancellationTokenSource = new CancellationTokenSource();
+
+			await SearchAsync();
     }
-  }
+
+		// This will be called from other components if this is the main page
+		private async Task RemoteSearchAsync(object data )
+		{
+			SearchQuery = data.ToString();
+			
+			Model = null;
+			StateHasChanged();
+
+			// Stop any search that was going on so this will be the main search 
+			// This still a bit buggy
+			cancellationTokenSource.Cancel();
+			cancellationTokenSource.Dispose();
+
+			cancellationTokenSource = new();
+
+			await SearchAsync();
+			StateHasChanged();
+		}
+
+		private async Task SearchAsync() 
+		{
+			if (!string.IsNullOrEmpty(SearchQuery))
+			{
+				Model = await YoutubeSearchService.SearchAsync(SearchQuery, cancellationTokenSource.Token);
+			}
+		}
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!disposedValue)
+			{
+				if (disposing)
+				{
+					PageNotificationService.RemoveActionCallBack(GetType());
+				}
+
+				disposedValue = true;
+			}
+		}
+
+		// TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+		~Search()
+		{
+			// Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+			Dispose(disposing: false);
+		}
+
+		public void Dispose()
+		{
+			// Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+			Dispose(disposing: true);
+			GC.SuppressFinalize(this);
+		}
+	}
 }
---
+++ MyMusic.Player/Services/PageNotificationService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MyMusic.Player.Services
+{
+	public static class PageNotificationService
+	{
+		private static readonly Dictionary<Type, Action<object>> _callbackDictionary;
+
+		static PageNotificationService() 
+		{
+			_callbackDictionary = [];
+		}
+
+		public static void AddActionCallBack(Type type, Action<object> callback)
+		{
+			_callbackDictionary.TryAdd(type, callback);
+		}
+
+		public static void RemoveActionCallBack(Type type) 
+		{
+			_callbackDictionary.Remove(type);
+		}
+
+		public static void InvokeActionCallBackFor(Type type, object data)
+		{
+			if( _callbackDictionary.TryGetValue(type, out var action) ) 
+			{
+				action.Invoke(data);
+			}
+		}
+	}
+}
