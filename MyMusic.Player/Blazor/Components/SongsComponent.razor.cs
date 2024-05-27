9854f836bf7b65f4783e5c838a8a2ce835c3e73c
mulder <borismulder@outlook.com>
2024-05-27 13:53:33 -0700

    Update SongsComponent.razor.cs

--- MyMusic.Player/Blazor/Components/SongsComponent.razor.cs
+++ MyMusic.Player/Blazor/Components/SongsComponent.razor.cs
@@ -1,20 +1,20 @@
 ﻿using Microsoft.AspNetCore.Components;
-using MyMusic.Player.Blazor.Models.Temp;
+using MyMusic.Player.Storage.Models;
 
 namespace MyMusic.Player.Blazor.Components
 {
   public partial class SongsComponent : ComponentBase
   {
-    private IEnumerable<SongDemo> songDemos;
+    private IEnumerable<Song> songDemos;
 
     protected override void OnInitialized()
     {
-      songDemos = Enumerable.Range(0,49).Select(i => new SongDemo 
+      songDemos = Enumerable.Range(0,49).Select(i => new Song
       {
         Serial = i,
         Description = $"Description_{i}",
-        Durration = TimeSpan.FromMinutes(i),
-        Name = $"Song_{i}"
+        Duration = i*60,
+        Title = $"Song_{i}"
       });
     }
   }
