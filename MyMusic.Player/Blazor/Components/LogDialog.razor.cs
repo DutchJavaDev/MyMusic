9f35632c815607b3ec3b4ceae6a7d791f7c70b37
mulder <borismulder@outlook.com>
2024-06-02 11:27:01 -0700

    fine tuned logging
    split in 2 classes read and write (try todo for all services)

---
+++ MyMusic.Player/Blazor/Components/LogDialog.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using MyMusic.Player.Storage.Models;
+using Radzen;
+
+namespace MyMusic.Player.Blazor.Components
+{
+	public partial class LogDialog : ComponentBase
+	{
+
+		[Inject]
+		private DialogService DialogService {  get; set; }
+
+		[Parameter]
+	  public Log Log { get; set; }
+	}
+}
--- MyMusic.Player/Storage/Models/Log.cs
+++ MyMusic.Player/Storage/Models/Log.cs
@@ -4,12 +4,12 @@ namespace MyMusic.Player.Storage.Models
 {
 	public sealed class Log
 	{
+		[AutoIncrement]
 		[PrimaryKey]
 		public int Serial { get; set; }
-		public byte Type { get; set; }
+		public byte LogType { get; set; }
 		public string Message {  get; set; }
 		public string StackTrace { get; set; }	
-		// Convert
-		public string DateTime { get; set; }
+		public DateTime DateTime { get; set; }
 	}
 }
