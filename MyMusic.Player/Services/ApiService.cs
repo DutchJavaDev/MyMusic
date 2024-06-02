15b251721229b52fb52d84082c533fcaa85454ed
mulder <borismulder@outlook.com>
2024-06-02 11:28:25 -0700

    added logging + notification

--- MyMusic.Player/Services/ApiService.cs
+++ MyMusic.Player/Services/ApiService.cs
@@ -1,4 +1,5 @@
 ﻿using MyMusic.Common.Models;
+using MyMusic.Player.Services.Write;
 using MyMusic.Player.Storage;
 using MyMusic.Player.Storage.Models;
 using Newtonsoft.Json;
@@ -7,108 +8,95 @@ using System.Web;
 
 namespace MyMusic.Player.Services
 {
-  public sealed class ApiService
-  {
-    private readonly IHttpClientFactory _httpClientFactory;
-    private readonly LocalDatabase _database;
-    private readonly LogService _logService;
+	public sealed class ApiService(IHttpClientFactory _httpClientFactory, 
+		LocalDatabase _database,
+		LogWriterService _logWriter)
+	{
 
-    public ApiService(IHttpClientFactory httpClientFactory,
-        LocalDatabase database,
-        LogService logService)
-    {
-      _database = database;
-      _httpClientFactory = httpClientFactory;
-      _logService = logService;
-    }
+		public async Task<IEnumerable<MusicDto>> GetDownloadedValuesAsync()
+		{
+			try
+			{
+				var configuration = await _database.GetServerConfigurationAsync();
 
-    public async Task<IEnumerable<MusicDto>> GetDownloadedValuesAsync()
-    {
-      try
-      {
-        var configuration = await _database.GetServerConfigurationAsync();
+				var client = await ConfigureApiClient(configuration);
 
-        var client = await ConfigureApiClient(configuration);
+				var request = await client.GetAsync("music");
 
-        var request = await client.GetAsync("music");
+				var stringContent = await request.Content.ReadAsStringAsync();
 
-        var stringContent = await request.Content.ReadAsStringAsync();
+				return JsonConvert.DeserializeObject<IEnumerable<MusicDto>>(stringContent);
+			}
+			catch (Exception e)
+			{
+				await _logWriter.Error(e, this);
+				return [];
+			}
+		}
 
-        return JsonConvert.DeserializeObject<IEnumerable<MusicDto>>(stringContent);
-      }
-      catch (Exception e)
-      {
-        await _logService.LogError(e, this);
+		public async Task<IEnumerable<StatusModel>> GetStatusModelsAsync()
+		{
+			try
+			{
+				/// "SERVER_AUTHENTICATION"
+				///
+				var configuration = await _database.GetServerConfigurationAsync();
 
-        throw;
-      }
-    }
+				var client = await ConfigureApiClient(configuration);
 
-    public async Task<IEnumerable<StatusModel>> GetStatusModelsAsync()
-    {
-      try
-      {
-        /// "SERVER_AUTHENTICATION"
-        ///
-        var configuration = await _database.GetServerConfigurationAsync();
+				var request = await client.GetAsync("download/status");
 
-        var client = await ConfigureApiClient(configuration);
+				var stringContent = await request.Content.ReadAsStringAsync();
 
-        var request = await client.GetAsync("download/status");
+				if (!request.IsSuccessStatusCode)
+				{
+					throw new Exception(stringContent);
+				}
 
-        var stringContent = await request.Content.ReadAsStringAsync();
+				return JsonConvert.DeserializeObject<IEnumerable<StatusModel>>(stringContent);
+			}
+			catch (Exception e)
+			{
+				await _logWriter.Error(e, this);
+				return [];
+			}
+		}
 
-        if(!request.IsSuccessStatusCode)
-        {
-          throw new Exception(stringContent);
-        }
+		public async Task<string> DownloadAsync(DownloadRequest downloadRequest)
+		{
+			try
+			{
+				// "SERVER_AUTHENTICATION"
+				//
+				var configuration = await _database.GetServerConfigurationAsync();
 
-        return JsonConvert.DeserializeObject<IEnumerable<StatusModel>>(stringContent);
-      }
-      catch (Exception e)
-      {
-        await _logService.LogError(e, this);
+				var client = await ConfigureApiClient(configuration);
 
-        throw;
-      }
-    }
+				var content = JsonConvert.SerializeObject(downloadRequest);
 
-    public async Task<string> DownloadAsync(DownloadRequest downloadRequest)
-    {
-      try
-      {
-        // "SERVER_AUTHENTICATION"
-        //
-        var configuration = await _database.GetServerConfigurationAsync();
+				// Encoding is import lol.........
+				var result = await client.PostAsync("download/start", new StringContent(content, Encoding.UTF8, "application/json"));
 
-        var client = await ConfigureApiClient(configuration);
+				return await result.Content.ReadAsStringAsync();
+			}
+			catch (Exception e)
+			{
+				await _logWriter.Error(e, this);
+				return string.Empty;
+			}
+		}
 
-        var content = JsonConvert.SerializeObject(downloadRequest);
+		private async Task<HttpClient> ConfigureApiClient(Configuration configuration)
+		{
+			if (string.IsNullOrEmpty(configuration.CloudUrl) || string.IsNullOrEmpty(configuration.CloudPassword))
+			{
+				throw new ArgumentException("Invalid configuration");
+			}
 
-        // Encoding is import lol.........
-        var result = await client.PostAsync("download/start", new StringContent(content, Encoding.UTF8, "application/json"));
-
-        return await result.Content.ReadAsStringAsync();
-      }
-      catch (Exception e)
-      {
-        await _logService.LogError(e, this);
-
-        throw;
-      }
-    }
-
-    private async Task<HttpClient> ConfigureApiClient(Configuration configuration)
-    {
-      if (string.IsNullOrEmpty(configuration.CloudUrl) || string.IsNullOrEmpty(configuration.CloudPassword))
-      {
-        throw new ArgumentException("Invalid configuration");
-      }
-
-      var client = _httpClientFactory.CreateClient();
-      client.BaseAddress = new Uri(await _database.GetBaseApiUrlAsync());
-      client.DefaultRequestHeaders.Authorization = new(HttpUtility.HtmlEncode(configuration.CloudPassword));
-      return client;
-    }
-  }
+			var client = _httpClientFactory.CreateClient();
+			client.BaseAddress = new Uri(await _database.GetBaseApiUrlAsync());
+			client.DefaultRequestHeaders.Authorization = new(HttpUtility.HtmlEncode(configuration.CloudPassword));
+			return client;
+		}
+	}
 }
