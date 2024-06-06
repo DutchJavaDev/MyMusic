74f55bbc94607b4397f689bd87fac6e7302b60d6
mulder <borismulder@outlook.com>
2024-06-06 07:44:36 -0700

    remove autoincrement attribute

--- MyMusic.Player/Storage/Models/SongStatus.cs
+++ MyMusic.Player/Storage/Models/SongStatus.cs
@@ -4,7 +4,6 @@ namespace MyMusic.Player.Storage.Models
 {
 	public sealed class SongStatus
 	{
-		[AutoIncrement]
 		[PrimaryKey]
 		public int Serial {  get; set; }
 		public int SongSerial { get; set; }
